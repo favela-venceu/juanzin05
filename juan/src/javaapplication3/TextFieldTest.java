bf15da53f656bf1d6c9c04bab72cdd15f2669e5f
favela-venceu <60987300+favela-venceu@users.noreply.github.com>
2020-03-11 18:26:17 -0400

    Add files via upload

---
+++ juan/src/javaapplication3/TextFieldTest.java
@@ -0,0 +1,14 @@
+package javaapplication3;
+import javax.swing.JFrame;
+public class TextFieldTest 
+{
+    public static void main (String[] args)
+    {
+        JavaApplication3 textFieldFrame = new JavaApplication3();
+        textFieldFrame.setDefaultCloseOperation ( JFrame.EXIT_ON_CLOSE );
+        textFieldFrame.setSize(350, 100 );
+        textFieldFrame.setVisible(true);
+        
+    }
+    
+}
